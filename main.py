b5ecb2e888ffdb28efcf93bc806511737bdafa93
MrAwesomeJr <58062648+MrAwesomeJr@users.noreply.github.com>
2022-04-14 13:43:52 +0800

    commented out unnecessary code (left in as an example)

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
 import songplayer
 
-lagtrain = songplayer.Song()
+# lagtrain = songplayer.Song()
+#
+# lagtrain_track = songplayer.converter.midi_to_data("Lagtrain.mid",
+#                                                    track=1,
+#                                                    truncate_silence=False,
+#                                                    transpose_semitones=-14,
+#                                                    round_beats=True)
+# print("track",1,"created")
+# lagtrain.append_song(lagtrain_track, beat_offset=0, inherit_bpm=True, inherit_time_signature=True)
+# print("track appended")
+#
+# for i in (3,4,5,6,7,13,14,15):
+#     lagtrain_track = songplayer.converter.midi_to_data("Lagtrain.mid",
+#                                                        track=i,
+#                                                        truncate_silence=False,
+#                                                        transpose_semitones=-2,
+#                                                        round_beats=True)
+#     print("track",i,"created")
+#     lagtrain.append_song(lagtrain_track, beat_offset=0, inherit_bpm=True, inherit_time_signature=True)
+#     print("track appended")
+#
+# lagtrain = lagtrain.sorted_song()
+# print("track sorted")
 
-lagtrain_track = songplayer.converter.midi_to_data("Lagtrain.mid",
-                                                       track=1,
-                                                       truncate_silence=False,
-                                                       transpose_semitones=-14,
-                                                       round_beats=True)
-print("track",1,"created")
-lagtrain.append_song(lagtrain_track, beat_offset=0, inherit_bpm=True, inherit_time_signature=True)
-print("track appended")
-
-for i in (3,5,7):
-    lagtrain_track = songplayer.converter.midi_to_data("Lagtrain.mid",
-                                                       track=i,
-                                                       truncate_silence=False,
-                                                       transpose_semitones=-2,
-                                                       round_beats=True)
-    print("track",i,"created")
-    lagtrain.append_song(lagtrain_track, beat_offset=0, inherit_bpm=True, inherit_time_signature=True)
-    print("track appended")
-
-lagtrain = lagtrain.sorted_song()
-print("track sorted")
-
-lagtrain.bpm = 100
-
-# file = open("songs/lagtrain_simplified","w+")
+# file = open("songs/lagtrain_full","w+")
 # file.write(songplayer.converter.data_to_text(lagtrain))
 # file.close()
 
-# file = open("songs/lagtrain_simplified","r")
-# lagtrain = songplayer.converter.text_to_data(file.read())
+file = open("songs/lagtrain_full","r")
+lagtrain = songplayer.converter.text_to_data(file.read())
 
 player = songplayer.TimedPlayer(lagtrain)
-player.play_lyre(countdown=1,debug=True)
+player.play_lyre(countdown=1, end_playback_beat=510, debug=True)
