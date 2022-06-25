a8d0c211dba60409bc82ccb6665f876b63025073
MrAwesomeJr <58062648+MrAwesomeJr@users.noreply.github.com>
2022-06-25 16:18:07 +0800

    added drum functionality

---
+++ songplayer/noteconverter.py
@@ -0,0 +1,64 @@
+import random
+
+
+class KeyboardConverter:
+    def __init__(self):
+        self.notes = {("C",2): "z",
+                      ("D",2): "x",
+                      ("E",2): "c",
+                      ("F",2): "v",
+                      ("G",2): "b",
+                      ("A",2): "n",
+                      ("B",2): "m",
+                      ("C",3): "a",
+                      ("D",3): "s",
+                      ("E",3): "d",
+                      ("F",3): "f",
+                      ("G",3): "g",
+                      ("A",3): "h",
+                      ("B",3): "j",
+                      ("C",4): "q",
+                      ("D",4): "w",
+                      ("E",4): "e",
+                      ("F",4): "r",
+                      ("G",4): "t",
+                      ("A",4): "y",
+                      ("B",4): "u"}
+
+    def note_to_key(self, note):
+        return self.notes[note]
+
+    def key_to_note(self, keyboard_note):
+        for key, value in self.notes:
+            if value == keyboard_note:
+                return value
+        return False
+
+
+class DrumConverter:
+    def __init__(self):
+        self.notes = {"C": random.choice(["A", "L"]),
+                      "D": random.choice(["S", "K"]),
+                      "E": "A",
+                      "F": "L",
+                      "G": "S",
+                      "A": "K"}
+
+        self.constant_notes = {"E": "A",
+                               "F": "L",
+                               "G": "S",
+                               "A": "K"}
+
+    def note_to_key(self, note):
+        return self.notes[note[0]]
+
+    def key_to_note(self, drum_note):
+        for key, value in self.constant_notes:
+            if value == drum_note:
+                return value
+        return False
+
+class NoteConverter:
+    def __init__(self):
+        self.keyboard = KeyboardConverter()
+        self.drum = DrumConverter()
--- songplayer/player.py
+++ songplayer/player.py
@@ -1,33 +1,15 @@
 import time
 import keyboard
+from noteconverter import NoteConverter
+
+default_countdown = 5
 
 
 class TimedPlayer:
     def __init__(self):
-        pass
+        self.noteconverter = NoteConverter()
 
-    def play_keyboard(self, song, countdown: int = 5, end_playback_beat=None, debug=False):
-        keyboard_notes = {("C",2): "z",
-                      ("D",2): "x",
-                      ("E",2): "c",
-                      ("F",2): "v",
-                      ("G",2): "b",
-                      ("A",2): "n",
-                      ("B",2): "m",
-                      ("C",3): "a",
-                      ("D",3): "s",
-                      ("E",3): "d",
-                      ("F",3): "f",
-                      ("G",3): "g",
-                      ("A",3): "h",
-                      ("B",3): "j",
-                      ("C",4): "q",
-                      ("D",4): "w",
-                      ("E",4): "e",
-                      ("F",4): "r",
-                      ("G",4): "t",
-                      ("A",4): "y",
-                      ("B",4): "u"}
+    def play_keyboard(self, song, countdown: int = default_countdown, end_playback_beat=None, debug=False):
         song = song.sorted_song()
         time.sleep(countdown)
         start_time = time.perf_counter()
@@ -36,15 +18,14 @@ class TimedPlayer:
         for note in song.data:
             if end_playback_beat != None and note.beat >= end_playback_beat:
                 break
-            if note.pitch in keyboard_notes:
+            if note.pitch in self.noteconverter.keyboard.notes:
                 beat_time = note.beat / (song.bpm / 60)
                 time_elapsed = time.perf_counter() - start_time
                 if beat_time <= time_elapsed:
                     previous_time = time_elapsed
                     if debug:
                         print(note,"dt:"+str(time_elapsed - beat_time))
-                    keyboard.press(keyboard_notes[note.pitch])
-                    keyboard.release(keyboard_notes[note.pitch])
+                    keyboard.press_and_release(self.noteconverter.keyboard.note_to_key(note.pitch))
 
                 else:
                     time_until_beat = beat_time - time_elapsed
@@ -55,37 +36,47 @@ class TimedPlayer:
 
                     if debug:
                         print(note,"dt:"+str(time_elapsed - beat_time))
-                    keyboard.press_and_release(keyboard_notes[note.pitch])
+                    keyboard.press_and_release(self.noteconverter.keyboard.note_to_key(note.pitch))
             elif debug:
                 print(str(note) + " Note Unplayable")
 
+    def play_drum(self, song, countdown: int = default_countdown, end_playback_beat=None, debug=False):
+        song = song.sorted_song()
+        time.sleep(countdown)
+        start_time = time.perf_counter()
+        time_elapsed = 0
+        previous_time = time_elapsed
+        for note in song.data:
+            if end_playback_beat != None and note.beat >= end_playback_beat:
+                break
+            if note.pitch in self.noteconverter.keyboard.notes:
+                beat_time = note.beat / (song.bpm / 60)
+                time_elapsed = time.perf_counter() - start_time
+                if beat_time <= time_elapsed:
+                    previous_time = time_elapsed
+                    if debug:
+                        print(note,"dt:"+str(time_elapsed - beat_time))
+                    keyboard.press_and_release(self.noteconverter.drum.note_to_key(note.pitch))
+
+                else:
+                    time_until_beat = beat_time - time_elapsed
+                    while time_until_beat > 0:
+                        time.sleep(time_until_beat / 2)
+                        time_elapsed = time.perf_counter() - start_time
+                        time_until_beat = beat_time - time_elapsed
+
+                    if debug:
+                        print(note,"dt:"+str(time_elapsed - beat_time))
+                    keyboard.press_and_release(self.noteconverter.drum.note_to_key(note.pitch))
+            elif debug:
+                print(str(note) + " Note Unplayable")
+
+
 class SmoothPlayer:
     def __init__(self):
-        pass
+        self.noteconverter = NoteConverter()
 
-    def play_keyboard(self, song, countdown: int = 5, end_playback_beat=None, debug=False):
-        keyboard_notes = {("C",2): "z",
-                      ("D",2): "x",
-                      ("E",2): "c",
-                      ("F",2): "v",
-                      ("G",2): "b",
-                      ("A",2): "n",
-                      ("B",2): "m",
-                      ("C",3): "a",
-                      ("D",3): "s",
-                      ("E",3): "d",
-                      ("F",3): "f",
-                      ("G",3): "g",
-                      ("A",3): "h",
-                      ("B",3): "j",
-                      ("C",4): "q",
-                      ("D",4): "w",
-                      ("E",4): "e",
-                      ("F",4): "r",
-                      ("G",4): "t",
-                      ("A",4): "y",
-                      ("B",4): "u"}
-
+    def play_keyboard(self, song, countdown: int = default_countdown, end_playback_beat=None, debug=False):
         song = song.sorted_song()
         time.sleep(countdown)
         beat_time = 0
@@ -93,7 +84,7 @@ class SmoothPlayer:
         for note in song.data:
             if end_playback_beat != None and note.beat >= end_playback_beat:
                 break
-            if note.pitch in keyboard_notes:
+            if note.pitch in self.noteconverter.keyboard.notes:
                 time_since_last_beat = time.perf_counter()
                 #init timer (actualized after sleep function)
                 previous_beat_time = beat_time
@@ -103,8 +94,32 @@ class SmoothPlayer:
                     time_since_last_beat -= time.perf_counter()
                     if debug:
                         print(note,"dt:"+str(beat_time - time_since_last_beat))
-                    keyboard.press_and_release(keyboard_notes[note.pitch])
+                    keyboard.press_and_release(self.noteconverter.keyboard.note_to_key(note.pitch))
                 else:
-                    keyboard.press_and_release(keyboard_notes[note.pitch])
+                    keyboard.press_and_release(self.noteconverter.keyboard.note_to_key(note.pitch))
+            elif debug:
+                print(str(note) + " Note Unplayable")
+
+    def play_drum(self, song, countdown: int = default_countdown, end_playback_beat=None, debug=False):
+        song = song.sorted_song()
+        time.sleep(countdown)
+        beat_time = 0
+        time_since_last_beat = 0
+        for note in song.data:
+            if end_playback_beat != None and note.beat >= end_playback_beat:
+                break
+            if note.pitch in self.noteconverter.keyboard.notes:
+                time_since_last_beat = time.perf_counter()
+                #init timer (actualized after sleep function)
+                previous_beat_time = beat_time
+                beat_time = note.beat / (song.bpm / 60)
+                if beat_time > previous_beat_time:
+                    time.sleep(beat_time - previous_beat_time)
+                    time_since_last_beat -= time.perf_counter()
+                    if debug:
+                        print(note,"dt:"+str(beat_time - time_since_last_beat))
+                    keyboard.press_and_release(self.noteconverter.drum.note_to_key(note.pitch))
+                else:
+                    keyboard.press_and_release(self.noteconverter.drum.note_to_key(note.pitch))
             elif debug:
                 print(str(note) + " Note Unplayable")
