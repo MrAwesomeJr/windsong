b7edd9d4076418203103d40ff19fb2f7867fc681
MrAwesomeJr <58062648+MrAwesomeJr@users.noreply.github.com>
2022-07-11 15:23:32 +0800

    fixed examples

--- windsong_examples/shelter_from_text_smooth.py
+++ windsong_examples/shelter_from_text_smooth.py
@@ -5,4 +5,4 @@ text = file.read()
 
 shelter = songplayer.converter.text_to_data(text)
 player = songplayer.Player(timer=songplayer.StaticTimer())
-player.play_keyboard(shelter, countdown=1)
+player.play(shelter, countdown=1)
