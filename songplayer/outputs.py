890c2bc5b716a32574e01cbfba081c1a62dceb7f
MrAwesomeJr <58062648+MrAwesomeJr@users.noreply.github.com>
2022-07-14 15:59:16 +0800

    added AutoSwitchKeyPress

--- songplayer/outputs.py
+++ songplayer/outputs.py
@@ -1,4 +1,10 @@
 import keyboard
+import os
+
+if os.name == "nt":
+    import win32gui
+    import win32con
+    import win32api
 
 
 class KeyPress:
@@ -8,6 +14,17 @@ class KeyPress:
     def press(self, key):
         keyboard.press_and_release(key)
 
+class AutoSwitchKeyPress:
+    def __init__(self):
+        if os.name != "nt":
+            raise ImportError("AutoSwitchKeyPress relies on win32.")
+
+        self.target_window = win32gui.FindWindow("UnityWndClass", None)
+        win32gui.SetForegroundWindow(self.target_window)
+
+
+    def press(self, key):
+        keyboard.press_and_release(key)
 
 class Simulator:
     def __init__(self):
